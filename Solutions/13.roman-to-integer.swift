9a0a56293d0f1c485597fcf374309889d1d755ef
miggelucas <lucasmigge@gmail.com>
2023-12-06 20:53:02 -0300

    romanToInteger solution

--- Solutions/13.roman-to-integer.swift
+++ Solutions/13.roman-to-integer.swift
@@ -7,7 +7,34 @@
 // @lc code=start
 class Solution {
     func romanToInt(_ s: String) -> Int {
+        var result: Int = 0
+
+        for (index, char) in s.enumerated() {
+            let nextIndex = s.index(s.startIndex, offsetBy: index + 1)
+            let next: Character? = index < s.count - 1 ? s[nextIndex] : nil
+
+            switch char {
+              case "I":
+                result += (next == "V" || next == "X") ? -1 : 1
+              case "V":
+                result += 5
+              case "X":
+                result += (next == "L" || next == "C") ? -10 : 10
+              case "L":
+                result += 50
+              case "C":
+                result += (next == "D" || next == "M") ? -100 : 100
+              case "D":
+                result += 500
+              case "M":
+                result += 1000
+             default:
+                break
+            }
         
+        }
+        
+        return result
     }
 }
 // @lc code=end
