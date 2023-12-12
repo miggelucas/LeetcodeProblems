3fa4c7128c5d349d578e87c38d21493311e10c6c
miggelucas <lucasmigge@gmail.com>
2023-12-12 13:43:56 -0300

    solving fibonacci again

---
+++ Solutions/94.binary-tree-inorder-traversal.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        if not root.left and not root.right:
+            return [root.val]
+        
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+        
+        
+# @lc code=end
+
